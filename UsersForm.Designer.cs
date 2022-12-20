f8402b88adac944b8224c54cac88e38f60334f2b
Pfumelelo Maluleke <Pfumelelo.Maluleke@standardbank.co.za>
2022-12-20 22:41:02 +0200

    Add project files.

---
+++ UsersForm.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace WinFormsApp1
+{
+    partial class UsersForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.MenuNoTwo = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnUnassign = new System.Windows.Forms.Button();
+            this.btnAssign = new System.Windows.Forms.Button();
+            this.txtHideID = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(0, 222);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(154, 20);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Assign Client Changes";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.RoyalBlue;
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button2.Location = new System.Drawing.Point(0, 1);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(800, 29);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Users";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // MenuNoTwo
+            // 
+            this.MenuNoTwo.BackColor = System.Drawing.Color.RoyalBlue;
+            this.MenuNoTwo.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.MenuNoTwo.Location = new System.Drawing.Point(0, 245);
+            this.MenuNoTwo.Name = "MenuNoTwo";
+            this.MenuNoTwo.Size = new System.Drawing.Size(800, 29);
+            this.MenuNoTwo.TabIndex = 12;
+            this.MenuNoTwo.TextAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.MenuNoTwo.UseVisualStyleBackColor = false;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AllowUserToAddRows = false;
+            this.dataGridView2.AllowUserToDeleteRows = false;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle2.Padding = new System.Windows.Forms.Padding(0, 0, 60, 0);
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView2.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(1, 36);
+            this.dataGridView2.MultiSelect = false;
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.ReadOnly = true;
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 29;
+            this.dataGridView2.RowTemplate.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView2.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.dataGridView2.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView2.Size = new System.Drawing.Size(800, 188);
+            this.dataGridView2.TabIndex = 6;
+            this.dataGridView2.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView2_CellContentClick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(212, 287);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 20);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Filter";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(459, 332);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(156, 20);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Assigned Client Group";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(34, 332);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(172, 20);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Unassigned Client Group";
+            // 
+            // listBox2
+            // 
+            this.listBox2.ItemHeight = 20;
+            this.listBox2.Location = new System.Drawing.Point(434, 355);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(366, 204);
+            this.listBox2.TabIndex = 17;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(277, 280);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(279, 27);
+            this.textBox1.TabIndex = 18;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
+            // 
+            // listBox1
+            // 
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(12, 355);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(354, 204);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // btnUnassign
+            // 
+            this.btnUnassign.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnUnassign.Location = new System.Drawing.Point(372, 458);
+            this.btnUnassign.Name = "btnUnassign";
+            this.btnUnassign.Size = new System.Drawing.Size(56, 41);
+            this.btnUnassign.TabIndex = 34;
+            this.btnUnassign.Text = "<";
+            this.btnUnassign.UseVisualStyleBackColor = true;
+            this.btnUnassign.Click += new System.EventHandler(this.btnUnassign_Click);
+            // 
+            // btnAssign
+            // 
+            this.btnAssign.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnAssign.Location = new System.Drawing.Point(372, 410);
+            this.btnAssign.Name = "btnAssign";
+            this.btnAssign.Size = new System.Drawing.Size(56, 42);
+            this.btnAssign.TabIndex = 33;
+            this.btnAssign.Text = ">";
+            this.btnAssign.UseVisualStyleBackColor = true;
+            this.btnAssign.Click += new System.EventHandler(this.btnAssign_Click);
+            // 
+            // txtHideID
+            // 
+            this.txtHideID.Location = new System.Drawing.Point(1064, 287);
+            this.txtHideID.Name = "txtHideID";
+            this.txtHideID.Size = new System.Drawing.Size(117, 27);
+            this.txtHideID.TabIndex = 35;
+            this.txtHideID.Visible = false;
+            // 
+            // UsersForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(813, 583);
+            this.Controls.Add(this.txtHideID);
+            this.Controls.Add(this.btnUnassign);
+            this.Controls.Add(this.btnAssign);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.MenuNoTwo);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView2);
+            this.Name = "UsersForm";
+            this.Load += new System.EventHandler(this.UsersForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private Label label1;
+        private Button button2;
+        private Button MenuNoTwo;
+        private DataGridView dataGridView2;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private ListBox listBox2;
+        private TextBox textBox1;
+        private ListBox listBox1;
+        private Button btnUnassign;
+        private Button btnAssign;
+        private TextBox txtHideID;
+    }
+}
---
+++ UsersForm.cs
@@ -0,0 +1,291 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace WinFormsApp1
+{
+    public partial class UsersForm : Form
+    {
+        private SqlConnection conn;
+
+        public UsersForm()
+        {
+            InitializeComponent();       
+        }
+
+        private void UsersForm_Load(object sender, EventArgs e)
+        {
+
+           conn = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Test;Integrated Security=True");
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = conn;
+
+            conn.Open();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = $" SELECT Id, Name ," +
+                              $" IIF(IsGlobalUser = 1, 'Global User', 'Not Global User') AS 'UserType' FROM Users";
+
+            cmd.ExecuteNonQuery();
+            DataTable dt = new DataTable();
+
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+      
+            dataGridView2.AutoGenerateColumns = false;
+
+            //Set Columns Count
+            dataGridView2.ColumnCount = 3;
+
+            //Add Columns
+            dataGridView2.Columns[0].Name = "Id";
+            dataGridView2.Columns[0].HeaderText = "User Id";
+            dataGridView2.Columns[0].DataPropertyName = "Id";
+            dataGridView2.Columns[0].Visible = false;
+
+            dataGridView2.Columns[1].HeaderText = "UserName";
+            dataGridView2.Columns[1].Name = "Name";
+            dataGridView2.Columns[1].DataPropertyName = "Name";
+            dataGridView2.Columns[1].Width = 380;
+
+            dataGridView2.Columns[2].Name = "UserType";
+            dataGridView2.Columns[2].HeaderText = "User Type";
+            dataGridView2.Columns[2].DataPropertyName = "UserType";
+            dataGridView2.Columns[2].Width = 380;
+            dataGridView2.DataSource = dt;
+
+            conn.Close();
+
+        
+        }
+
+        private void dataGridView2_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+            txtHideID.Clear();
+            var rowIndex = e.RowIndex;
+            var currentCellValue = dataGridView2.Rows[rowIndex].Cells[1].Value.ToString();
+            MenuNoTwo.Text = "Assign User to Client Group - " + currentCellValue;
+            txtHideID.Text = dataGridView2.Rows[rowIndex].Cells[0].Value.ToString();
+
+            conn = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Test;Integrated Security=True");
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = conn;
+
+            conn.Open();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = $" SELECT c.Name from Users a INNER JOIN ClientGroupUsers b ON a.Id = b.UserId INNER JOIN ClientGroups c ON b.ClientGroupId = c.Id Where a.Id  = {txtHideID.Text}";
+           
+            cmd.ExecuteNonQuery();
+            DataTable dt = new DataTable();
+
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                DataRow dr = dt.Rows[i];
+
+                listBox2.Items.Add(dr[0]);               
+            }
+
+            conn.Close();
+
+
+            SqlCommand cmd2 = new SqlCommand();
+            cmd2.Connection = conn;
+            conn.Open();
+            cmd2.CommandType = CommandType.Text;
+            cmd2.CommandText = $" select Distinct b.Name FROM ClientGroupUsers a INNER JOIN ClientGroups b ON a.ClientGroupId = b.Id Where b.Name NOT IN (select b.Name FROM ClientGroupUsers a INNER JOIN ClientGroups b ON a.ClientGroupId = b.Id Where a.userId = {txtHideID.Text})";
+
+            cmd2.ExecuteNonQuery();
+            DataTable dt2 = new DataTable();
+
+            SqlDataAdapter da2 = new SqlDataAdapter(cmd2);
+            da2.Fill(dt2);
+
+            for (int i = 0; i < dt2.Rows.Count; i++)
+            {
+                DataRow dr2 = dt2.Rows[i];
+
+                listBox1.Items.Add(dr2[0]);      
+            }
+
+            conn.Close();
+        }
+
+        List<string> registrationsList = new List<string>();
+        List<string> listCollection = new List<string>();
+        public void AssignUsers()
+        {
+            listBox1.Items.Clear();
+            conn = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Test;Integrated Security=True");
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = conn;
+
+            conn.Open();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = $" SELECT Distinct b.Name FROM ClientGroupUsers a INNER JOIN ClientGroups b ON a.ClientGroupId = b.Id Where b.Name NOT IN (select b.Name FROM ClientGroupUsers a INNER JOIN ClientGroups b ON a.ClientGroupId = b.Id Where a.userId = {txtHideID.Text})";
+
+            cmd.ExecuteNonQuery();
+            DataTable dt = new DataTable();
+
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            listCollection.Clear();
+            registrationsList.Clear();
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                DataRow dr = dt.Rows[i];
+
+                listCollection.Add((string)dr[0]);
+            }
+            registrationsList = listCollection.ToList();
+
+            if (!string.IsNullOrEmpty(textBox1.Text))
+            {
+                listBox1.Items.Clear();
+                foreach (string str in registrationsList)
+                {
+                    if (str.StartsWith(textBox1.Text))
+                    {
+                        listBox1.Items.Add(str);
+                    }
+                }
+            }
+            else if (textBox1.Text == "")
+            {
+                listBox1.Items.Clear();
+                for (int i = 0; i < dt.Rows.Count; i++)
+                {
+                    DataRow dr = dt.Rows[i];
+
+                    listBox1.Items.Add(dr[0]);
+                }
+            }
+            conn.Close();
+        }
+
+        public void UnAssignUsers()
+        {
+            listBox2.Items.Clear();
+            conn = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Test;Integrated Security=True");
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = conn;
+
+            conn.Open();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = $" SELECT c.Name from Users a INNER JOIN ClientGroupUsers b ON a.Id = b.UserId INNER JOIN ClientGroups c ON b.ClientGroupId = c.Id Where a.Id  = {txtHideID.Text}";
+
+            cmd.ExecuteNonQuery();
+            DataTable dt = new DataTable();
+
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            listCollection.Clear();
+            registrationsList.Clear();
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                DataRow dr = dt.Rows[i];
+
+                listCollection.Add((string)dr[0]);
+            }
+            registrationsList = listCollection.ToList();
+
+            if (!string.IsNullOrEmpty(textBox1.Text))
+            {
+                listBox2.Items.Clear();
+                foreach (string str in registrationsList)
+                {
+                    if (str.StartsWith(textBox1.Text))
+                    {
+                        listBox2.Items.Add(str);
+                    }
+                }
+            }
+            else if (textBox1.Text == "")
+            {
+                listBox2.Items.Clear();
+                for (int i = 0; i < dt.Rows.Count; i++)
+                {
+                    DataRow dr = dt.Rows[i];
+
+                    listBox2.Items.Add(dr[0]);
+                }
+            }
+            conn.Close();
+        }
+     
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+            AssignUsers();
+            UnAssignUsers();
+        }
+
+        private void btnAssign_Click(object sender, EventArgs e)
+        {
+            string selectedClientGroup;
+            if (listBox1.SelectedItems.Count > 0)
+            {
+
+                selectedClientGroup = listBox2.SelectedItems[0].ToString();
+
+                conn = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Test;Integrated Security=True");
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = conn;
+
+                conn.Open();
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = $" INSERT INTO ClientGroupUsers VALUES( (Select Id From ClientGroups Where Name = '{selectedClientGroup}'), {txtHideID.Text})";
+                cmd.ExecuteNonQuery();
+                conn.Close();
+
+                AssignUsers();
+                UnAssignUsers();           
+            }
+            else
+            {
+                MessageBox.Show("No Select User Client Group");
+            }
+        }
+
+        private void btnUnassign_Click(object sender, EventArgs e)
+        {
+
+            string selectedClientGroup;
+            if (listBox1.SelectedItems.Count > 0)
+            {
+
+                selectedClientGroup = listBox1.SelectedItems[0].ToString();
+
+                conn = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Test;Integrated Security=True");
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = conn;
+
+                conn.Open();
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = $" DELETE FROM ClientGroupUsers WHERE UserId = {txtHideID.Text} and Id =  (Select Id From ClientGroups Where Name = '{selectedClientGroup}')";
+                cmd.ExecuteNonQuery();
+                conn.Close();
+                txtHideID.Clear();
+
+                AssignUsers();
+                UnAssignUsers();
+            }
+            else
+            {
+                MessageBox.Show("No Select User Client Group");
+            }         
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
